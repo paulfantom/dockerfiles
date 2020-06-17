f6206f1ff7d4a6f13774663f3cad7987de66ff29
paulfantom <pawel@krupa.net.pl>
2020-06-17 12:23:22 +0200

    restic-mysql: better timout handling

--- restic-mysql/entrypoint.sh
+++ restic-mysql/entrypoint.sh
@@ -12,23 +12,29 @@ PUSHGATEWAY_URL="${PUSHGATEWAY_URL:-""}"
 INSTANCE="${INSTANCE:-""}"
 NAMESPACE="${NAMESPACE:-""}"
 
-TIMEOUT="10m"
+TIMEOUT="${TIMEOUT:-"60m"}"
 
 set -euo pipefail
+
+timeout_handler() {
+	if [ "$?" -eq 124 ]; then
+		echo "ERROR: one of restic commands timed out. Try changing TIMEOUT env to higher value."
+	fi
+}
 
 backup() {
 	local start end data stats snapshots group
 
-	echo "$(date +"%F %T") INFO: Releasing all locks"
-	if ! timeout $TIMEOUT restic unlock --remove-all -v; then
+	echo "INFO: Releasing all locks"
+	if ! timeout "$TIMEOUT" restic unlock --remove-all -v; then
 		echo "$(date +"%F %T") INFO: creating new repository"
-		timeout $TIMEOUT restic init
+		timeout "$TIMEOUT" restic init
 	fi
 
-	echo "$(date +"%F %T") INFO: checking repository state"
-	timeout 60m restic check
+	echo "INFO: checking repository state"
+	timeout "$TIMEOUT" restic check
 
-	echo "$(date +"%F %T") INFO: starting new backup"
+	echo "INFO: starting new backup"
 	start=$(date +%s)
 	if [ -n "$DATA_DIRECTORY" ]; then
 		restic backup ${RESTIC_ARGS} --host "${INSTANCE:-"$(hostname)"}" "${DATA_DIRECTORY}"
@@ -40,35 +46,38 @@ backup() {
 	fi
 	end=$(date +%s)
 
+	# statistics are not imporatant when not sent to monitoring
+	if [ -z "$PUSHGATEWAY_URL" ]; then
+		echo "INFO: PUSHGATEWAY_URL not defined, metrics won't be sent"
+		exit 0
+	fi
+
 	# statistics
-	echo "$(date +"%F %T") INFO: Backup finished, exporting statistics"
-	stats=$(timeout $TIMEOUT restic stats --no-lock --json)
+	echo "INFO: Backup finished, exporting statistics"
+	stats=$(timeout "$TIMEOUT" restic stats --no-lock --json)
 	if [ "$stats" = "" ]; then
-		echo "$(date +"%F %T") ERROR: No backup statistics can be found. Exiting."
+		echo "ERROR: No backup statistics can be found. Exiting."
 		exit 1
 	fi
 
 	# snapshots
-	snapshots=$(timeout $TIMEOUT restic snapshots --no-lock --json | jq length)
+	snapshots=$(timeout "$TIMEOUT" restic snapshots --no-lock --json | jq length)
 	if [ "$snapshots" = "" ]; then
-		echo "$(date +"%F %T") ERROR: No backup snapshots can be found. Exiting."
+		echo "ERROR: No backup snapshots can be found. Exiting."
 		exit 1
 	fi
 
-	if [ -z "$PUSHGATEWAY_URL" ]; then
-		echo "INFO: PUSHGATEWAY_URL not defined, metrics won't be sent"
-	else
-		group="job/backup"
-		if [ "${INSTANCE}" != "" ]; then
-			group="${group}/instance@base64/$(echo -n "${INSTANCE}" | base64)"
-		fi
-		if [ "${NAMESPACE}" != "" ]; then
-			group="${group}/namespace@base64/$(echo -n "${NAMESPACE}" | base64 )"
-		fi
-		group="${group}/repository@base64/$(echo -n "${RESTIC_REPOSITORY}" | base64 )"
-		group="${group}/data@base64/$(echo -n "${data}" | base64 )"
+	group="job/backup"
+	if [ "${INSTANCE}" != "" ]; then
+		group="${group}/instance@base64/$(echo -n "${INSTANCE}" | base64)"
+	fi
+	if [ "${NAMESPACE}" != "" ]; then
+		group="${group}/namespace@base64/$(echo -n "${NAMESPACE}" | base64 )"
+	fi
+	group="${group}/repository@base64/$(echo -n "${RESTIC_REPOSITORY}" | base64 )"
+	group="${group}/data@base64/$(echo -n "${data}" | base64 )"
 
-		cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}/metrics/${group}" 2> /dev/null
+	cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}/metrics/${group}" 2> /dev/null
 # HELP backup_start_last_timestamp Time whan backup started
 # TYPE backup_start_last_timestamp counter
 backup_start_last_timestamp ${start}
@@ -85,8 +94,7 @@ backup_files_total $(echo "$stats" | jq .total_file_count)
 # TYPE backup_snapshots_total gauge
 backup_snapshots_total ${snapshots}
 EOF
-		echo "$(date +"%F %T") INFO: Statistics exported. All done."
-	fi
+	echo "INFO: Statistics exported. All done."
 }
 
 check_db_vars() {
@@ -120,7 +128,9 @@ check_restic_vars() {
 	fi
 }
 
-echo "$(date +"%F %T") INFO: Start restic backup"
+echo "INFO: Start restic backup"
 check_restic_vars
+
+trap timeout_handler EXIT
 
 backup
